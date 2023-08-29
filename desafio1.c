ffd91e25415482abb99d9544fc23229c000b3e26
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-08-29 11:19:30 -0300

    Add files via upload

---
+++ desafio1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+main(){
+
+ // ler a media do aluno
+ // se media >= 7.0 - "aprovado"
+ // senão media <4.0 - "reprovado"
+ // senão media >=4.0 < 7.0 - "recuperacao"
+
+  float mediaDoAluno;
+
+  printf("Digite a media do aluno");
+  scanf("%f", &mediaDoAluno);
+  
+  if(mediaDoAluno >= 7.0){
+
+    printf("Aprovado");
+
+
+  }else if(mediaDoAluno < 4.0){
+    printf("Reprovado");
+
+  }else(mediaDoAluno >= 4.0 < 7.0){
+
+    printf("Recuperação");
+
+  }
+
+
+
+
+
+
+
+
+
+ 
+}
